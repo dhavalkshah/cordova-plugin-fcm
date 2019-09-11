819681b81ec0cc023b62ce38b55f782d7f1a96c6
Dhaval Shah <dhaval@pratishthanventures.com>
2019-09-12 00:21:53 +0530

    Overloading unsubscribe method

--- src/android/FCMPlugin.java
+++ src/android/FCMPlugin.java
@@ -5,6 +5,7 @@ import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaInterface;
 import android.util.Log;
+import android.content.Context;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -14,6 +15,8 @@ import android.os.Bundle;
 
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.crashlytics.android.Crashlytics;
 
 import java.util.Map;
 
@@ -26,15 +29,19 @@ public class FCMPlugin extends CordovaPlugin {
 	public static String tokenRefreshCallBack = "FCMPlugin.onTokenRefreshReceived";
 	public static Boolean notificationCallBackReady = false;
 	public static Map<String, Object> lastPush = null;
+	private FirebaseAnalytics mFirebaseAnalytics;
 	 
 	public FCMPlugin() {}
 	
 	public void initialize(CordovaInterface cordova, CordovaWebView webView) {
 		super.initialize(cordova, webView);
+		final Context context = this.cordova.getActivity().getApplicationContext();
 		gWebView = webView;
 		Log.d(TAG, "==> FCMPlugin initialize");
 		FirebaseMessaging.getInstance().subscribeToTopic("android");
 		FirebaseMessaging.getInstance().subscribeToTopic("all");
+		mFirebaseAnalytics = FirebaseAnalytics.getInstance(context);
+		mFirebaseAnalytics.setAnalyticsCollectionEnabled(true);
 	}
 	 
 	public boolean execute(final String action, final JSONArray args, final CallbackContext callbackContext) throws JSONException {
@@ -86,16 +93,18 @@ public class FCMPlugin extends CordovaPlugin {
 				});
 			}
 			else if (action.equals("unsubscribeFromTopic")) {
-				cordova.getThreadPool().execute(new Runnable() {
-					public void run() {
-						try{
-							FirebaseMessaging.getInstance().unsubscribeFromTopic( args.getString(0) );
-							callbackContext.success();
-						}catch(Exception e){
-							callbackContext.error(e.getMessage());
-						}
-					}
-				});
+				this.isUnsubscribeFromTopic(callbackContext, args.getString(0));
+				return true;
+				// cordova.getThreadPool().execute(new Runnable() {
+				// 	public void run() {
+				// 		try{
+				// 			FirebaseMessaging.getInstance().unsubscribeFromTopic( args.getString(0) );
+				// 			callbackContext.success();
+				// 		}catch(Exception e){
+				// 			callbackContext.error(e.getMessage());
+				// 		}
+				// 	}
+				// });
 			}
 			else{
 				callbackContext.error("Method not found");
@@ -120,6 +129,65 @@ public class FCMPlugin extends CordovaPlugin {
         //});
 		return true;
 	}
+	private void isUnsubscribeFromTopic(final CallbackContext callbackContext, final String name){
+		Log.d(TAG,"isUnsubscribeFromTopic: Data is:"+name);
+		try{
+			JSONObject param = new JSONObject(name);
+			String func = param.has("func")?param.getString("func"):"";
+			if(func.equals("logEvent")){
+				String eventName = param.getString("eventName");
+				param.remove("func");
+				param.remove("eventName");
+				this.logEvent(callbackContext, eventName, param);
+			}
+			else{
+				String topic = param.getString("topic");
+				this.unsubscribeFromTopic(callbackContext, topic);
+			}
+		}
+		catch(Exception e){
+			this.unsubscribeFromTopic(callbackContext, name);
+		}
+	}
+	private void unsubscribeFromTopic(final CallbackContext callbackContext, final String name){
+		Log.d(TAG,"unsubscribeFromTopic: Trying to unsubscribe a topic");
+		cordova.getThreadPool().execute(new Runnable() {
+			public void run() {
+				try{
+					FirebaseMessaging.getInstance().unsubscribeFromTopic( name );
+					callbackContext.success();
+				}catch(Exception e){
+					callbackContext.error(e.getMessage());
+				}
+			}
+		});
+	}
+	private void logEvent(final CallbackContext callbackContext, final String name, final JSONObject params) throws JSONException {
+		Log.d(TAG,"logEvent: Trying to logEvent :"+name);
+        final Bundle bundle = new Bundle();
+        Iterator iter = params.keys();
+        while (iter.hasNext()) {
+            String key = (String) iter.next();
+            Object value = params.get(key);
+
+            if (value instanceof Integer || value instanceof Double) {
+                bundle.putFloat(key, ((Number) value).floatValue());
+            } else {
+                bundle.putString(key, value.toString());
+            }
+        }
+        cordova.getThreadPool().execute(new Runnable() {
+            public void run() {
+                try {
+                    mFirebaseAnalytics.logEvent(name, bundle);
+                    callbackContext.success();
+                } catch (Exception e) {
+                    Crashlytics.logException(e);
+                    callbackContext.error(e.getMessage());
+                }
+            }
+        });
+    }
 	
 	public static void sendPushPayload(Map<String, Object> payload) {
 		Log.d(TAG, "==> FCMPlugin sendPushPayload");
